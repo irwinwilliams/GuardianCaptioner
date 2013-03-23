200b5bbe97fbd346ed2b60accb3b713e24ed497d
Irwin <irwin.williams@gmail.com>
2013-03-23 18:53:06 -0300

    Update captioner.js
    Added stuff to remove the htly iframe stuff. 

--- captioner.js
+++ captioner.js
@@ -8,9 +8,44 @@
     else {
         getCaptions();
     }
+
+    function showHTLYRemoveOption() {
+        var body = $("body");
+        body.append("<div  title='One more step.' id='htlyOverthrower'>Hey, if you look at the top of this page, you'll see a toolbar and some twitter-related stuff, you're not actually on the Guardian site, would you like to go there?<div>");
+        $("#htlyOverthrower").dialog({
+            dialogClass: "no-close",
+            resizable: false,
+            height: 200,
+            modal: true,
+            buttons: {
+                "Remove that toolbar :)": function () {
+                    $(this).dialog("close");
+                    window.location = $("#hootFrame").attr("src");
+                },
+                "Cancel :(": function () {
+                    $(this).dialog("close");
+                }
+            }
+        });
+    }
+
     function getCaptions() {
-        m = $("img[typeof='foaf:Image']");
-        $(m).hover(showCaption, hideCaption);
+        htly = document.getElementById("hootFrame");
+        if (!htly) {
+            m = $("img[typeof='foaf:Image']");
+            $(m).hover(showCaption, hideCaption);
+        }
+        else {
+            link = document.createElement("link");
+            link.rel = "stylesheet";
+            link.href = "http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css";
+            document.head.appendChild(link);
+
+            script = document.createElement('script');
+            script.src = 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.js';
+            script.onload = showHTLYRemoveOption;
+            document.body.appendChild(script);
+        }
     }
     function showCaption() {
         var par = $(this).parent();
@@ -30,6 +65,6 @@
         sp.css("margin-top", "-" + sp.height() + "px");
         sp.css(":hover", "text-decoration:none !important");
     }
-}());
+} ());
 
 //http://www.guardian.co.tt/gallery/talk-trinidad-09-march-2013
